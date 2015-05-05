0a81502cfb4aa31b5963bd30a3e80d41c8052514
Maksym Bilenko <sath891@gmail.com>
2015-05-05 15:23:43 +0300

    Move environment variables configuration at entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,9 @@
 chown -R oracle:dba /u01/app/oracle
 # Update hostname
 sed -i -E "s/HOST = [^)]+/HOST = $HOSTNAME/g" /u01/app/oracle/product/11.2.0/xe/network/admin/listener.ora
+echo "export ORACLE_HOME=/u01/app/oracle/product/11.2.0/xe" >> /etc/bash.bashrc
+echo "export PATH=$ORACLE_HOME/bin:$PATH" >> /etc/bash.bashrc
+echo "export ORACLE_SID=XE" >> /etc/bash.bashrc
 
 case "$1" in
 	'')
@@ -12,9 +15,6 @@ case "$1" in
 			echo "found files in /u01/app/oracle/oradata Using them instead of initial database"
 		else
 			echo "Database not initialized. Initializing database."
-			echo "export ORACLE_HOME=/u01/app/oracle/product/11.2.0/xe" >> /etc/bash.bashrc
-			echo "export PATH=$ORACLE_HOME/bin:$PATH" >> /etc/bash.bashrc
-			echo "export ORACLE_SID=XE" >> /etc/bash.bashrc
 			printf 8080\\n1521\\noracle\\noracle\\ny\\n | /etc/init.d/oracle-xe configure
 			echo "Database initialized. Please visit http://#containeer:8080/apex to proceed with configuration"
 		fi
