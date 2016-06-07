604c96e9b44d7b4c8776676db3a3b7901f500d7b
wmw <wnameless@gmail.com>
2016-06-07 12:43:07 +0800

    Fix ssh login bug

--- assets/setup.sh
+++ assets/setup.sh
@@ -10,9 +10,9 @@ apt-get update
 apt-get install -y openssh-server &&
 mkdir /var/run/sshd &&
 echo 'root:admin' | chpasswd &&
-sed -i 's/PermitRootLogin without-password/PermitRootLogin yes/' /etc/ssh/sshd_config &&
-sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd &&
-echo "export VISIBLE=now" >> /etc/profile &&
+sed -i 's/^PermitRootLogin .*/PermitRootLogin yes/' /etc/ssh/sshd_config &&
+sed -i 's/session\s*required\s*pam_loginuid.so/session optional pam_loginuid.so/g' /etc/pam.d/sshd &&
+echo 'export VISIBLE=now' >> /etc/profile &&
 
 # Prepare to install Oracle
 apt-get install -y libaio1 net-tools bc &&
