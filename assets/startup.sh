25c689e8139901e1d4173a702b77bc99c6afe8d5
wmw <wnameless@gmail.com>
2016-06-11 16:32:20 +0800

    Fix tnsnames.ora bug

--- assets/startup.sh
+++ assets/startup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-LISTENERS_ORA=/u01/app/oracle/product/11.2.0/xe/network/admin/listener.ora
+LISTENER_ORA=/u01/app/oracle/product/11.2.0/xe/network/admin/listener.ora
+TNSNAMES_ORA=/u01/app/oracle/product/11.2.0/xe/network/admin/tnsnames.ora
 
-cp "${LISTENERS_ORA}.tmpl" "$LISTENERS_ORA" &&
-sed -i "s/%hostname%/$HOSTNAME/g" "${LISTENERS_ORA}" &&
-sed -i "s/%port%/1521/g" "${LISTENERS_ORA}" &&
+cp "${LISTENER_ORA}.tmpl" "$LISTENER_ORA" &&
+sed -i "s/%hostname%/$HOSTNAME/g" "${LISTENER_ORA}" &&
+sed -i "s/%port%/1521/g" "${LISTENER_ORA}" &&
+cp "${TNSNAMES_ORA}.tmpl" "$TNSNAMES_ORA" &&
+sed -i "s/%hostname%/$HOSTNAME/g" "${TNSNAMES_ORA}" &&
+sed -i "s/%port%/1521/g" "${TNSNAMES_ORA}" &&
 
 service oracle-xe start
 
