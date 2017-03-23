57326a0d65ff5441faf97913bd102bcaef56fd9a
Lucas Rodrigues <lucas.rodrigues@stf.jus.br>
2017-03-22 23:54:18 -0300

    Update configure_registry.sh

--- assets/configure_registry.sh
+++ assets/configure_registry.sh
@@ -2,7 +2,9 @@
 
 set -e
 
+echo -n | openssl s_client -connect registry.stf.jus.br:443 -showcerts 2>/dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > ca.crt
+
 sudo mkdir -p /etc/docker/certs.d/registry.stf.jus.br
-sudo cp ./shared/certificates/ca.crt /etc/docker/certs.d/registry.stf.jus.br/ca.crt
+sudo cp ./ca.crt /etc/docker/certs.d/registry.stf.jus.br/ca.crt
 sudo ls /etc/docker/certs.d/registry.stf.jus.br/
 sudo service docker restart
