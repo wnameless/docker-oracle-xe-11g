63cdb4bfab4a42e15774dce3fa571bcd1466a2d1
Lucas Rodrigues <lucas.rodrigues@stf.jus.br>
2017-03-22 23:58:55 -0300

    Adicionado certificado

--- assets/configure_registry.sh
+++ assets/configure_registry.sh
@@ -2,9 +2,7 @@
 
 set -e
 
-echo -n | openssl s_client -connect registry.stf.jus.br:443 -showcerts 2>/dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > ca.crt
-
 sudo mkdir -p /etc/docker/certs.d/registry.stf.jus.br
-sudo cp ./ca.crt /etc/docker/certs.d/registry.stf.jus.br/ca.crt
+sudo cp ./assets/ca.crt /etc/docker/certs.d/registry.stf.jus.br/ca.crt
 sudo ls /etc/docker/certs.d/registry.stf.jus.br/
 sudo service docker restart
