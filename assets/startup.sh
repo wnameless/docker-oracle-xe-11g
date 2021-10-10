16089e60e8ac8892ca4388c160307b7c3fa1d61d
Michael Voříšek <mvorisek@mvorisek.cz>
2021-10-10 20:42:41 +0200

    Use "set -e" for handling errors in sh scripts

--- assets/startup.sh
+++ assets/startup.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
+
+set -e
+
 LISTENER_ORA=/u01/app/oracle/product/11.2.0/xe/network/admin/listener.ora
 TNSNAMES_ORA=/u01/app/oracle/product/11.2.0/xe/network/admin/tnsnames.ora
 
-cp "${LISTENER_ORA}.tmpl" "$LISTENER_ORA" &&
-sed -i "s/%hostname%/$HOSTNAME/g" "${LISTENER_ORA}" &&
-sed -i "s/%port%/1521/g" "${LISTENER_ORA}" &&
-cp "${TNSNAMES_ORA}.tmpl" "$TNSNAMES_ORA" &&
-sed -i "s/%hostname%/$HOSTNAME/g" "${TNSNAMES_ORA}" &&
-sed -i "s/%port%/1521/g" "${TNSNAMES_ORA}" &&
+cp "${LISTENER_ORA}.tmpl" "$LISTENER_ORA"
+sed -i "s/%hostname%/$HOSTNAME/g" "${LISTENER_ORA}"
+sed -i "s/%port%/1521/g" "${LISTENER_ORA}"
+cp "${TNSNAMES_ORA}.tmpl" "$TNSNAMES_ORA"
+sed -i "s/%hostname%/$HOSTNAME/g" "${TNSNAMES_ORA}"
+sed -i "s/%port%/1521/g" "${TNSNAMES_ORA}"
 
 service oracle-xe start
 
